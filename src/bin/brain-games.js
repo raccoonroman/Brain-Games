001156309e0b9bbca9ea1c91b71be7db3080e3c3
Роман <kromanjs@gmail.com>
2018-12-26 17:41:01 +0200

    step 4

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import greeting from '..';
+import { welcome, greeting } from '..';
 
-console.log('Welcome to the Brain Games! \n');
-
+welcome();
 greeting();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+
+export const welcome = () => {
+  console.log('Welcome to the Brain Games! \n');
+};
+
+
+export const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}! \n`);
+};
+
+
+export const isEven = () => {
+  console.log('Answer "yes" if number even otherwise answer "no". \n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}! \n`);
+
+  const attemptAmt = 3;
+
+  for (let i = 0; i < attemptAmt; i += 1) {
+    const randomNum = Math.floor(Math.random() * 1000);
+    console.log(`Question: ${randomNum}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const currectAnswer = randomNum % 2 === 0 ? 'yes' : 'no';
+
+    if (currectAnswer !== userAnswer) {
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${currectAnswer}"`);
+      break;
+    } else {
+      console.log('Correct!');
+      if (i === 2) {
+        console.log(`Congratulations, ${userName}!`);
+      }
+    }
+  }
 };
