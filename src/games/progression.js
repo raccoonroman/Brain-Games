288976c3c74e6b3ecc7d70155bb3f25d4d7c1a75
Роман <kromanjs@gmail.com>
2018-12-29 17:33:32 +0200

    corrected comments

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,22 @@ import { cons } from 'hexlet-pairs';
 import engine from '..';
 
 const gameDescription = 'What number is missing in the progression?';
-const progressionStart = 1;
 const progressionLength = 10;
 
 const getAnswerAndQuestion = () => {
-  const startNumber = getRandomInteger(1, 20);
+  const firstNumber = getRandomInteger(1, 20);
   const step = getRandomInteger(6, 20);
-  const hiddenElementPosition = getRandomInteger(progressionStart, progressionLength);
-  let questionWithFirstSpace = '';
-  for (let i = progressionStart; i <= progressionLength; i += 1) {
+  const hiddenElementPosition = getRandomInteger(0, progressionLength - 1);
+  let question = '';
+  const correctAnswer = `${firstNumber + step * hiddenElementPosition}`;
+  for (let i = 0; i < progressionLength; i += 1) {
     if (hiddenElementPosition === i) {
-      questionWithFirstSpace += ' ..';
+      question += '.. ';
     } else {
-      questionWithFirstSpace += ` ${startNumber + step * i}`;
+      question += `${firstNumber + step * i} `;
     }
   }
-  const question = questionWithFirstSpace.slice(1);
-  const correctAnswer = `${startNumber + step * hiddenElementPosition}`;
-  return cons(question, correctAnswer);
+  return cons(question.trim(), correctAnswer);
 };
 
 export default () => engine(gameDescription, getAnswerAndQuestion);
